cfb5bcf6fee165b6be5fa8f86a5884d3115fa613
Alfred Wemali <alfredwemali05@gmail.com>
2023-08-09 11:28:31 +0300

    Places screen

---
+++ lib/screens/places.dart
@@ -0,0 +1,17 @@
+import 'package:favorite_places/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your places'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
+      body: PlacesList(places: []),
+    );
+  }
+}
